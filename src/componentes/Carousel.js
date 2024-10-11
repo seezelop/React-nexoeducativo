f7ea3dccc3d3b0e11f9fb8ce6cde3d05fb804f8e
seezelop <sebastianlopez1984@gmail.com>
2024-10-11 19:42:07 -0300

    Corrijo className

--- src/componentes/Carousel.js
+++ src/componentes/Carousel.js
@@ -4,30 +4,46 @@ import React, { Component } from "react";
 class Carousel extends Component {
     render() {
         return (
-            <div id="carouselExampleIndicators" class="carousel slide">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div id="carouselExampleIndicators" className
+="carousel slide">
+                <div className
+="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className
+="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="imageduca.JPG" class="d-block w-100" alt="..." />
+                <div className
+="carousel-inner">
+                    <div className
+="carousel-item active">
+                        <img src="imageduca.JPG" className
+="d-block w-100" alt="..." />
                     </div>
-                    <div class="carousel-item">
-                        <img src="Captura2.JPG" class="d-block w-100" alt="..." />
+                    <div className
+="carousel-item">
+                        <img src="Captura2.JPG" className
+="d-block w-100" alt="..." />
                     </div>
-                    <div class="carousel-item">
-                        <img src="Captura3.JPG" class="d-block w-100" alt="..." />
+                    <div className
+="carousel-item">
+                        <img src="Captura3.JPG" className
+="d-block w-100" alt="..." />
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                <button className
+="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className
+="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className
+="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                <button className
+="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className
+="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className
+="visually-hidden">Next</span>
                 </button>
             </div>
         );
