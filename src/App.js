7c7506f344499d697070997f2ab4f23d7b8f3bba
seezelop <sebastianlopez1984@gmail.com>
2024-10-11 19:03:25 -0300

    Agrego Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './componentes/Header';
 import Carousel from './componentes/Carousel';
-
+import Footer from './componentes/Footer';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <div className="container my-4">
       <Carousel />
-        
+      <Footer />
       </div>
       
     </div>
