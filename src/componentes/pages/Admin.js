b5d62e1b150c8b9562d19f770a9479be0db3ac09
seezelop <sebastianlopez1984@gmail.com>
2024-10-16 09:05:35 -0300

    corrijo estilos

--- src/componentes/pages/Admin.js
+++ src/componentes/pages/Admin.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 function Admin() {
   return (
-    <div className="container">
-      <h1>Bienvenido Administrador</h1>
-      <p>Esta es la página del administrador. Aquí podrás gestionar los usuarios y administrar la plataforma.</p>
-    </div>
+    <section className="d-flex flex-column min-vh-100"> {/* Contenedor que ocupa toda la altura */}
+      <div className="container flex-grow-1"> {/* Flex-grow para que crezca y ocupe espacio */}
+        <h1>Bienvenido Administrador</h1>
+        <p>Esta es la página del administrador. Aquí podrás gestionar los usuarios y administrar la plataforma.</p>
+      </div>
+      <footer className="bg-dark text-light text-center py-3 mt-auto"> {/* Footer con mt-auto para estar al final */}
+        <p>© 2024 Copyright: Todos los derechos reservados.</p>
+      </footer>
+    </section>
   );
 }
 
--- src/componentes/pages/User.js
+++ src/componentes/pages/User.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 function User() {
   return (
-    <div className="container">
-      <h1>Bienvenido Usuario Común</h1>
-      <p>Esta es la página para usuarios comunes. Aquí podrás ver información general.</p>
-    </div>
+    <section className="d-flex flex-column min-vh-100"> {/* Contenedor que ocupa toda la altura */}
+      <div className="container flex-grow-1"> {/* Flex-grow para que crezca y ocupe espacio */}
+        <h1>Bienvenido Usuario Común</h1>
+        <p>Esta es la página para usuarios comunes. Aquí podrás ver información general.</p>
+      </div>
+      <footer className="bg-dark text-light text-center py-3 mt-auto"> {/* Footer con mt-auto para estar al final */}
+        <p>© 2024 Copyright: Todos los derechos reservados.</p>
+      </footer>
+    </section>
   );
 }
 
 export default User;
+
