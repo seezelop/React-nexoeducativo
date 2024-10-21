221e510960320090a773b100531052f5643f2937
MartinaAsad <lasupermartu15@gmail.com>
2024-10-21 17:34:07 -0300

    seccion preguntas frecuentes hecha

--- src/componentes/pages/QueOfrecemos.js
+++ src/componentes/pages/QueOfrecemos.js
@@ -3,19 +3,47 @@ import React from 'react';
 function QueOfrecemos() {
   return (
     <section className="d-flex flex-column min-vh-100"> {/* Contenedor principal usando section */}
-      <section className="container d-flex justify-content-center align-items-center flex-grow-1">
+      <section className="container d-flex justify-content-center flex-grow-1">
         <section className="col-md-8">
           <h2 className="text-center mb-4">¿Qué ofrecemos?</h2>
-          <p className="text-center">
-            Aquí puedes describir los servicios y productos que ofreces.
-            Por ejemplo, podrías hablar sobre:
-          </p>
-          <ul>
-            <li>Servicio 1: Descripción del servicio 1.</li>
-            <li>Servicio 2: Descripción del servicio 2.</li>
-            <li>Servicio 3: Descripción del servicio 3.</li>
-            {/* Agrega más servicios según sea necesario */}
-          </ul>
+          <div class="accordion" id="accordionExample">
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
         </section>
       </section>
     </section>
