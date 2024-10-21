4dcda680572e9f4ec98d283140815298856726f5
seezelop <sebastianlopez1984@gmail.com>
2024-10-21 18:32:53 -0300

    Actualizo mensaje

--- src/componentes/pages/Admin.js
+++ src/componentes/pages/Admin.js
@@ -6,7 +6,7 @@ function Admin() {
   return (
     <section className="d-flex flex-column min-vh-100"> {/* Contenedor que ocupa toda la altura */}
           <div className="container flex-grow-1"> {/* Flex-grow para que crezca y ocupe espacio */}
-        <h1>Bienvenido</h1>
+        <h1>Bienvenido Administrador</h1>
         <FormEditarSuperAdmin/>
       </div>
       
