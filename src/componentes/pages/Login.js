af745337e4376199d4389fd3b5090375c58c7102
seezelop <sebastianlopez1984@gmail.com>
2024-10-29 20:39:36 -0300

    subo probable codigo (comentado) para login con back end

--- src/componentes/pages/Login.js
+++ src/componentes/pages/Login.js
@@ -73,3 +73,79 @@ function Login() {
 }
 
 export default Login;
+
+
+/*
+
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';  // Importa el contexto
+import axios from 'axios'; // Asegúrate de tener axios instalado
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { setUserRole } = useContext(UserContext); // Accede a setUserRole desde el contexto
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/api/usuario/login', { // Ajusta la URL según tu API
+        eMail: email,
+        clave: password,
+      });
+
+      const userData = response.data;
+      setUserRole(userData.rol);  // Guarda el rol en el contexto
+
+      if (userData.rol === "admin") {
+        navigate('/admin'); // Redirige a la página del admin
+      } else {
+        navigate('/user');  // Redirige a la página de usuario común
+      }
+    } catch (error) {
+      setError("Error al iniciar sesión: " + error.response.data || error.message);
+    }
+  };
+
+  return (
+    <section className="d-flex flex-column min-vh-100">
+      <section className="container d-flex justify-content-center align-items-center flex-grow-1">
+        <section className="col-md-6">
+          <h2 className="text-center mb-4">Iniciar sesión</h2>
+          <form onSubmit={handleSubmit}>
+            <section className="mb-3">
+              <label htmlFor="email" className="form-label">Correo electrónico</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Ingrese su correo"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </section>
+            <section className="mb-3">
+              <label htmlFor="password" className="form-label">Contraseña</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Ingrese su contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </section>
+            {error && <p className="text-danger">{error}</p>}
+            <button type="submit" className="btn btn-primary w-100 mb-4">Iniciar sesión</button>
+          </form>
+        </section>
+      </section>
+    </section>
+  );
+}
+
+export default Login;
+*/
