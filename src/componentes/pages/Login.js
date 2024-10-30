dc46cc270df06c2c3bdca88488b8bb0625c8a34f
MartinaAsad <lasupermartu15@gmail.com>
2024-10-29 22:14:31 -0300

    solucione el login

--- src/componentes/pages/Login.js
+++ src/componentes/pages/Login.js
@@ -84,7 +84,7 @@ import axios from 'axios';
 
 function Login() {
   const [mail, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [clave, setClave] = useState('');
   const [error, setError] = useState('');
   const { setUserRole } = useContext(UserContext); // Accede a setUserRole desde el contexto
   const navigate = useNavigate();
@@ -94,13 +94,13 @@ function Login() {
     try {
       const response = await axios.post('http://localhost:8080/auth/login', { // Conecta con la API
         mail: mail, 
-        clave: password,
+        clave: clave,
       });
 
       const userData = response.data;
       setUserRole(userData.rol);  // Guarda el rol en el contexto
 
-      if (userData.rol === "admin") {
+      if (userData.rol === "super admin") {
         navigate('/admin'); // Redirige a la página del admin
       } else {
         navigate('/user');  // Redirige a la página de usuario común
@@ -109,7 +109,7 @@ function Login() {
       if (error.response) {
         setError("Error al iniciar sesión: " + error.response.data); // Mostrar el mensaje de error del servidor
       } else {
-        setError("Error al iniciar sesión: " + error.message); // Error general
+        setError("Error 2 al iniciar sesión: " + error.message); // Error general
       }
     }
   };
@@ -139,8 +139,8 @@ function Login() {
                 className="form-control"
                 id="password"
                 placeholder="Ingrese su contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={clave}
+                onChange={(e) => setClave(e.target.value)}
                 required
               />
             </section>
