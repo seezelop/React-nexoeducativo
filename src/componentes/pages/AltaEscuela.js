87fcfef9c145fed6fe7816b109e8736ff278e26f
MartinaAsad <lasupermartu15@gmail.com>
2024-11-03 22:52:04 -0300

    version nueva SIN PROBAR de AltaEscuela

--- src/componentes/pages/AltaEscuela.js
+++ src/componentes/pages/AltaEscuela.js
@@ -1,82 +1,56 @@
-import React, { Component, useState } from 'react';
-import { DropdownButton, Dropdown } from 'react-bootstrap'; // Asegúrate de tener esto importado si lo usas
+import React, { Component } from 'react'; 
+import { DropdownButton, Dropdown } from 'react-bootstrap'; 
 import axios from 'axios';
+
 class AltaEscuela extends Component {
     //se setean las propiedades que van a ser enviadas
     constructor(props) {
-        super(props);
-        this.state = {
-            nombre: '',
-            direccion: '',
-            activo: 1,
-            planSeleccionado: '',
-            idPlan: null,
-            jefeColegioSeleccionado: '',
-            idJefe: null,
-            planes: [],
-            jefesColegio: []
-        };
-    }
-
+         super(props);
+          this.state =
+           { nombre: '',
+             direccion: '', 
+             activo: 1, 
+             planSeleccionado: '', 
+             idPlan: null, 
+             jefeColegioSeleccionado: '', 
+             idJefe: null, 
+             planes: [], 
+             jefesColegio: [] }; 
+            }
     //esto es para rellenar los tipos de plan
     cargarPlanes = async () => {
         try {
-            const respuesta = await fetch(`http://localhost:8080/api/usuario/getNombrePlanes`);
-            console.log(respuesta);
-
-            if (respuesta.status === 200) {
-                const datos = await respuesta.json();
-                console.log("datos planes:", datos);
-
-                // Procesa los datos para convertirlos en un formato de objeto
-                
-                this.setState({  });
-
-            } else if (respuesta.status === 404) {
-                console.log('no hay planes');
-            } else {
-                console.log('Hubo un error y no sabemos que pasó');
-            }
+            const response = await axios.get('http://localhost:8080/usuario/getNombrePlanes');
+            this.setState({ planes: response.data });
         } catch (error) {
-            console.log(error);
+            console.error('Error al cargar los planes:', error);
         }
     };
 
     //esto es para rellenar que muestre los jefes colegios sin colegios asignados
     cargarJefeColegio = async () => {
         try {
-            const respuesta = await fetch(`http://localhost:8080/api/usuario/getJefeColegioSinEscuela`);
-
-            console.log(respuesta);
-
-            // Si la respuesta es correcta
-            if (respuesta.status === 200) {
-                const datos = await respuesta.json();
-                console.log("datitos:" + datos);
-
-                // Mapea los datos a los elementos del dropdown
-                const jefesColegio = datos.map(jefe => ({
+            const response = await axios.get('http://localhost:8080/usuario/getJefeColegioSinEscuela');
+            if (response.status === 200) {
+                const jefesColegio = response.data.map(jefe => ({
                     id: jefe.id_usuario,
                     nombre: `${jefe.nombre} ${jefe.apellido}`
                 }));
-
-                // Establece los items en el estado
                 this.setState({ jefesColegio });
-
-
-            } else if (respuesta.status === 404) {
+            } else if (response.status === 404) {
                 console.log('Todos los jefes colegios registrados tienen un colegio asignado');
             } else {
                 console.log('Hubo un error y no sabemos que paso');
             }
         } catch (error) {
-            console.log(error);
+            console.error('Error al cargar los jefes de colegio:', error);
         }
     };
-    //aca hago que se llame a los metodos en el dropdown
+
+    // Llama a los métodos para cargar datos al montar el componente
     componentDidMount() {
-        this.cargarJefeColegio();
         this.cargarPlanes();
+        this.cargarJefeColegio();
     }
 
     // Manejar el cambio en los inputs
@@ -86,9 +60,14 @@ class AltaEscuela extends Component {
     };
 
     // Manejar selección en Dropdown
-    
-    // Enviar los datos al backend
-   
+    handleDropdownChange = (field, value) => {
+        const parsedValue = JSON.parse(value);
+        this.setState({
+            [`${field}Seleccionado`]: parsedValue.nombre,
+            [`id${field.charAt(0).toUpperCase() + field.slice(1)}`]: parsedValue.id
+        });
+    };
+
     render() {
         //valores por defecto
         const {
@@ -103,7 +82,7 @@ class AltaEscuela extends Component {
             <section className="d-flex flex-column">
                 {/* Contenedor principal usando section */}
                 <section className="container d-flex justify-content-center align-items-center flex-grow-1">
-                    <section className="col-lg-12"> {/* Ajustamos a 12 columnas para mejor visibilidad */}
+                    <section className="col-lg-12">
                         <form onSubmit={this.handleSubmit}>
                             <div className="row">
                                 {/* Campo para el Nombre */}
@@ -120,7 +99,7 @@ class AltaEscuela extends Component {
                                     />
                                 </div>
                             </div>
-                            {/* Campo para la direccion */}
+                            {/* Campo para la dirección */}
                             <div className="row">
                                 <div className="col mb-3">
                                     <label htmlFor="direccion" className="form-label">{direccion}</label>
@@ -136,59 +115,41 @@ class AltaEscuela extends Component {
                                 </div>
                             </div>
 
-
-                            {/* Campo para verificar si esta activo */}
-
-
                             {/* Campo desplegable para los tipos de Plan */}
                             <div className="pt-2 pb-5">
                                 <label htmlFor="dropdown-basic-button" className="form-label">{plan}</label>
                                 <DropdownButton
                                     id="dropdown-plan"
                                     title={this.state.planSeleccionado || "Seleccione un Plan"}
-                                    onSelect={(value) => this.handleDropdownChange('planSeleccionado', value)}
+                                    onSelect={(value) => this.handleDropdownChange('plan', value)}
                                 >
-                                    {/*<Dropdown.Menu>
-                                        {this.state.planes.map(plan => (
-                                            <Dropdown.Item
-                                                key={plan.id}
-                                                eventKey={JSON.stringify({ id: plan.idPlan, nombre: plan.descripcion })}
-                                            >
-                                                {plan.descripcion}
-                                            </Dropdown.Item>
-                                        ))}
-                                    </Dropdown.Menu>*/}
                                     {this.state.planes.map(plan => (
                                         <Dropdown.Item
-                                            key={plan.id}
-                                            eventKey={JSON.stringify({ id: plan.id, nombre: plan.nombre })}
+                                            key={plan.idPlan}
+                                            eventKey={JSON.stringify({ id: plan.idPlan, nombre: plan.descripcion })}
                                         >
-                                            {plan.nombre}
+                                            {plan.descripcion}
                                         </Dropdown.Item>
                                     ))}
                                 </DropdownButton>
-
                             </div>
 
                             {/* Campo desplegable para el jefe colegio */}
-
                             <div className="pb-5">
                                 <label htmlFor="dropdown-basic-button" className="form-label">{jefeColegio}</label>
                                 <DropdownButton
                                     id="dropdown-jefeC"
                                     title={this.state.jefeColegioSeleccionado || "Seleccione un Jefe Colegio"}
-                                    onSelect={(value) => this.handleDropdownChange('jefeColegioSeleccionado', value)}
+                                    onSelect={(value) => this.handleDropdownChange('jefeColegio', value)}
                                 >
-                                    <Dropdown.Menu>
-                                        {this.state.jefesColegio.map(jefe => (
-                                            <Dropdown.Item
-                                                key={jefe.id}
-                                                eventKey={JSON.stringify({ id: jefe.id, nombre: jefe.nombre })}
-                                            >
-                                                {jefe.nombre}
-                                            </Dropdown.Item>
-                                        ))}
-                                    </Dropdown.Menu>
+                                    {this.state.jefesColegio.map(jefe => (
+                                        <Dropdown.Item
+                                            key={jefe.id}
+                                            eventKey={JSON.stringify({ id: jefe.id, nombre: jefe.nombre })}
+                                        >
+                                            {jefe.nombre}
+                                        </Dropdown.Item>
+                                    ))}
                                 </DropdownButton>
                             </div>
 
@@ -202,15 +163,4 @@ class AltaEscuela extends Component {
         );
     }
 }
-
-// Valores por defecto para las props
-AltaEscuela.defaultProps = {
-    nombre: "Nombre:",
-    direccion: "Dirección:",
-    email: "Email:",
-    plan: "Tipo de plan:",
-    jefeColegio: "Jefe colegio:",
-    buttonText: "Confirmar"
-};
-
 export default AltaEscuela;
