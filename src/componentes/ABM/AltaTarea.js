9b56d886011a807f0c82bc54048d8705c3a0501a
MartinaAsad <lasupermartu15@gmail.com>
2025-01-18 19:28:33 -0300

    complete con un ednpoint faltante en altaTarea

--- src/componentes/ABM/AltaTarea.js
+++ src/componentes/ABM/AltaTarea.js
@@ -4,6 +4,27 @@ function AltaTarea() {
   const [titulo, setTitulo] = useState('');
   const [descripcion, setDescripcion] = useState('');
 
+  cargarMaterias = async () => {
+    try {
+        const response = await axios.get(`http://localhost:8080/api/usuario/selecMateriaProfesor/{cursoIdCurso}`, {
+            withCredentials: true
+        });
+
+        //console.log("info alumnos api: "+JSON.stringify(response.data))
+
+        const materias = response.data.map(materia => ({
+            idUsuario: materia.id_usuario,
+            nombre: `${alumno.nombre} ${alumno.apellido}`
+        }));
+
+        this.setState({ alumnos });
+        console.log("Alumnos cargados: ", alumnos);
+
+    } catch (error) {
+        console.error('Error al cargar los alumnos:', error);
+    }
+};
+
   const handleSubmit = (e) => {
     e.preventDefault();
     // Aquí iría la lógica para guardar la tarea
