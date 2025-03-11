606a4522f34e2256ec47f7c64711786e2be70a77
seezelop <sebastianlopez1984@gmail.com>
2025-03-11 20:09:38 -0300

    corrijo

--- src/componentes/pages/ChatIndividual.js
+++ src/componentes/pages/ChatIndividual.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import WebSocketService from "../context/WebSocketService";
+import WebSocketService from "../../context/WebSocketService";
 
 const ChatIndividual = () => {
   const { mail } = useParams();
