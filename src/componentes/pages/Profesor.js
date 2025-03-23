b4c30eeec4054a6b83d247f8c51a65c342217525
seezelop <sebastianlopez1984@gmail.com>
2025-03-23 16:45:15 -0300

    actualizo

--- src/componentes/pages/Profesor.js
+++ src/componentes/pages/Profesor.js
@@ -7,9 +7,7 @@ function Profesor() {
         <h1>Bienvenido Profesor</h1>
         <p>Accede a los materiales educativos, asigna calificaciones y gestiona actividades de los alumnos.</p>
       </div>
-      <footer className="bg-dark text-light text-center py-3 mt-auto">
-        <p>© 2024 Copyright: Todos los derechos reservados.</p>
-      </footer>
+
     </section>
   );
 }
