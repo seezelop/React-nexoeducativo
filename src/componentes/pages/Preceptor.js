33c5ecfd192c931ad55cf5f4e5b6eb5a3f4abb39
seezelop <sebastianlopez1984@gmail.com>
2025-03-24 15:54:27 -0300

    Elimino footer de mas

--- src/componentes/pages/Preceptor.js
+++ src/componentes/pages/Preceptor.js
@@ -7,9 +7,6 @@ function Preceptor() {
         <h1>Bienvenido Preceptor</h1>
         <p>Consulta la información de los estudiantes y gestiona la asistencia y reportes diarios.</p>
       </div>
-      <footer className="bg-dark text-light text-center py-3 mt-auto">
-        <p>© 2024 Copyright: Todos los derechos reservados.</p>
-      </footer>
     </section>
   );
 }
